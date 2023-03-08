6d509e2eb2bbe034c6b0e935b7f0ac22ec8f6bcc
liangfeng <liangfeng@qihangxingchen.com>
2023-03-08 11:06:17 +0800

    w1-1

---
+++ w1-1/add.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+contract Counter {
+ uint public counter;
+    constructor() {
+ counter = 0;
+ }
+ function count() public {
+ counter = counter + 1;
+ }
+ 
+function add(uint value) public{
+   counter = counter + value;
+}
+}
